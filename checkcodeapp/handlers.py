8d452c4ce606fea7009d88955d1a784c1e7f7e17
niyoufa <1061794187@qq.com>
2016-10-25 14:17:03 +0800

    fix bug

--- checkcodeapp/handlers.py
+++ checkcodeapp/handlers.py
@@ -1,54 +1,34 @@
 #coding=utf-8
 import datetime,pdb
+import tornado
 from tornado.options import options
 from dxb.handler import TokenAPIHandler,APIHandler,ListCreateAPIHandler,\
     RetrieveUpdateDestroyAPIHandler
 import libs.utils as utils
 import libs.wslib as wslib
+import libs.asynclib as asynclib
+async_server = asynclib.AsyncUtils(20)
 import models
 
 class MobileCheckCode(APIHandler):
     model = models.CheckCode()
 
+    @property
+    def executor(self):
+        return self.application.executor
+
+    @tornado.web.asynchronous
+    @tornado.gen.coroutine
     def get(self):
         result = utils.init_response_data()
         checkcode_coll = self.model.get_coll()
         try:
             mobile = self.get_argument("mobile")
-            self.model.check_mobile(mobile)
-            curr_time = datetime.datetime.now()
-            if checkcode_coll.find({"mobile":mobile,"enable_flag":True}).count() > 0:
-                # 验证码请求限制 每小时限制5条
-                if checkcode_coll.find({"mobile":mobile,
-                        "add_time":{
-                            "$gte":curr_time - datetime.timedelta(hours=1),
-                            "$lte":curr_time + datetime.timedelta(hours=1),
-                        }
-                    }).count() >= 5:
-                    raise Exception("验证码请求限制，每小时限制5条！")
-
-                cr = checkcode_coll.find({"mobile":mobile,"enable_flag":True})
-                for checkcode in cr:
-                    checkcode["enable_flag"] = False
-                    checkcode_coll.save(checkcode)
-            else:
-                pass
-            random_code = utils.get_random_num(6,mode="number")
-
-            checkcode_coll.insert_one({
-                "mobile":mobile,
-                "enable_flag":True,
-                "add_time":curr_time,
-                "type":"mobile",
-                "code":random_code,
-            })
-            # res = wslib.send_msg(mobile,"尊敬的用户您好，您本次的验证码为%s,30分钟内有效"%random_code)
-            # if res != "0" :
-            #     raise ValueError("错误代码：%s"%res)
-
-            # res = self.model.send_106sms(mobile, random_code)
-            # if res.get("message","") != "ok":
-            #     raise Exception('短信发送失败,请联系开发人员\n错误信息：%s'%res)
+            yield async_server.cmd(self.model.check_mobile, mobile)
+            yield self.executor.submit(self.model.check_limit, (mobile))
+            random_code = utils.get_random_num(6, mode="number")
+            yield async_server.cmd(self.model.send_kaixintong_sms, mobile, random_code)
+            yield self.model.save(mobile,random_code,result)
             result["data"]["code"] = random_code
         except Exception, e:
             result = utils.reset_response_data(0, str(e))
@@ -61,8 +41,11 @@ class MobileCheckCode(APIHandler):
         try:
             phone = self.get_argument("phone","")
             phone_code = self.get_argument("phone_code","")
-            if phone != "":
-                utils.check_code(checkcode_coll, phone, phone_code)
+            if phone == "":
+                raise Exception("请输入手机号！")
+            elif phone_code == "":
+                raise Exception("请输入验证码！")
+            utils.check_code(checkcode_coll, phone, phone_code)
         except Exception, e:
             result = utils.reset_response_data(0, str(e))
         self.finish(result)
--- checkcodeapp/models.py
+++ checkcodeapp/models.py
@@ -1,9 +1,11 @@
 #coding=utf-8
 
-import sys, urllib, urllib2, json,pdb
+import sys, urllib, urllib2, json,pdb, datetime
+import tornado
 
 import libs.modellib as model
 import libs.utils as utils
+import libs.xmllib as xmllib
 
 class CheckCode(model.BaseModel,model.Singleton):
     __name = "dxb.checkcode"
@@ -22,6 +24,25 @@ class CheckCode(model.BaseModel,model.Singleton):
             print(content)
         return json.loads(content)
 
+    def send_kaixintong_sms(self,mobile,code):
+        url = "http://101.201.41.194:9999/sms.aspx"
+        content = u"【尚得公益】你的验证码是%s，有效时间30分钟，请不要告诉他人"%code
+        data = {
+            "userid":"",
+            "account":"",
+            "password":"",
+            "mobile":mobile,
+            "content":content,
+            "action":"send",
+        }
+        data = urllib.urlencode(data)
+        opener = urllib2.build_opener()
+        xml_res = opener.open(url, data).read()
+        res = xmllib.Xml2Json(xml_res).result
+        # if res.get("returnsms", {}).get("returnstatus") != 'Success' \
+        #         and res.get("returnsms", {}).get("message") != 'ok':
+        #     raise Exception('短信发送失败,请联系开发人员\n错误信息：%s' % res)
+
     def check_mobile(self,mobile):
         phoneprefix = ['130', '131', '132', '133', '134', '135', '136', '137', '138', '139', '150', '151', '152', '153',
                        '156', '158', '159', '170', '183', '182', '185', '186', '188', '189']
@@ -33,4 +54,40 @@ class CheckCode(model.BaseModel,model.Singleton):
                     raise Exception("手机号无效！")
             else:
                 raise Exception("手机号必须是数字！")
+
+    def check_limit(self,mobile):
+        curr_time = datetime.datetime.now()
+        if self.coll.find({"mobile": mobile, "enable_flag": True}).count() > 0:
+            # 验证码请求限制 每小时限制5条
+            if self.coll.find({"mobile": mobile,
+                                    "add_time": {
+                                        "$gte": curr_time - datetime.timedelta(hours=1),
+                                        "$lte": curr_time + datetime.timedelta(hours=1),
+                                    }
+                                    }).count() >= 5:
+                raise Exception("验证码请求限制，每小时限制5条！")
+
+            # 将之前未使用的验证码设置为过期
+            cr = self.coll.find({"mobile": mobile, "enable_flag": True})
+            for checkcode in cr:
+                checkcode["enable_flag"] = False
+                self.coll.save(checkcode)
+
+    @tornado.gen.coroutine
+    def save(self,mobile,random_code,result):
+        future = tornado.concurrent.Future()
+        def callback(mobile,random_code):
+            curr_time = datetime.datetime.now()
+            res = self.coll.insert_one({
+                "mobile": mobile,
+                "enable_flag": True,
+                "add_time": curr_time,
+                "type": "mobile",
+                "code": random_code,
+            })
+            future.set_result(res)
+
+        tornado.ioloop.IOLoop.instance().add_callback(callback, mobile, random_code)
+        result["insert"] = yield future
+        result["insert"] = utils.dump(result["insert"])
 
